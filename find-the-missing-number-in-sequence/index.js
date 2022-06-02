95c8ab27428902f0a024986cb709616cf5f46dbb
zmardil <btzardil@gmail.com>
2022-06-02 23:03:02 +0530

    Optimized: If missing start & end values

--- find-the-missing-number-in-sequence/index.js
+++ find-the-missing-number-in-sequence/index.js
@@ -1,24 +1,29 @@
-const findMissingNumber = (array) => {
-    // min = array[0];
-    // max = array[array.length - 1];
+const findMissingNumber = (array, actualLength) => {
+    
 
-    /** 
-     * If the start & end numbers are different
-     * Alternate: for loop - 0(n) 
-     */
-    max = Math.max(...array);
-    min = Math.min(...array); 
+    if(actualLength - array.length != 1) return null;
+
+    // const min = array[0];
+    // const max = array[array.length - 1];
+
+    const max = Math.max(...array);
+    const min = Math.min(...array); 
 
     // Summation without the missing value
-    sum = 0;
+    const sum = 0;
     for(let number of numberSeq) sum += number;
 
     // Summation with the missing value
-    trueSum = Math.floor((max - min + 1) * (min + max) / 2);
+    const trueSum = Math.floor((max - min + 1) * (min + max) / 2);
 
-    return trueSum - sum;
+    // If the start & end value are missing
+    if(trueSum - sum == 0) {
+        return [min - 1, max + 1];
+    } else {
+        return trueSum - sum;
+    }
 }
 
-numberSeq = [21,25,29,28,22,24,27,26,30];
+const numberSeq = [21, 25, 29, 28, 22, 24, 27, 26, 30];
 
-console.log(findMissingNumber(numberSeq));
+console.log(findMissingNumber(numberSeq, 10));
