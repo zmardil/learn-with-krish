a3fc98cd208539c922dec8809ba29a7246fcb149
zmardil <btzardil@gmail.com>
2022-06-03 21:49:34 +0530

    Bug fixes & added log() method

--- find-the-missing-number-in-sequence/index.js
+++ find-the-missing-number-in-sequence/index.js
@@ -1,21 +1,20 @@
 const findMissingNumber = (array, actualLength) => {
     
-
     if(actualLength - array.length != 1) return null;
-
+    
     // const min = array[0];
     // const max = array[array.length - 1];
-
+    
     const max = Math.max(...array);
     const min = Math.min(...array); 
-
+    
     // Summation without the missing value
-    const sum = 0;
-    for(let number of numberSeq) sum += number;
-
+    let sum = 0;
+    for(let number of array) sum += number;
+    
     // Summation with the missing value
     const trueSum = Math.floor((max - min + 1) * (min + max) / 2);
-
+    
     // If the start & end value are missing
     if(trueSum - sum == 0) {
         return [min - 1, max + 1];
@@ -24,6 +23,10 @@ const findMissingNumber = (array, actualLength) => {
     }
 }
 
+const log = (output) => console.log(Array.isArray(output) ? `${output[0]} or ${output[1]}` : output);
+
 const numberSeq = [21, 25, 29, 28, 22, 24, 27, 26, 30];
 
-console.log(findMissingNumber(numberSeq, 10));
+log(findMissingNumber(numberSeq, 10));
+log(findMissingNumber([23, 25, 29, 28, 22, 24, 27, 26, 30], 10));
+log(findMissingNumber([21, 25, 29, 28, 22, 24, 27, 26, 23], 10));
